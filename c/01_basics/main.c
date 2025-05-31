80675ba132245f1c1c7d63100714569b59cfd242
gonza56d <gonza56d@gmail.com>
2025-05-31 09:19:34 -0300

    Ask for user input

--- c/01_basics/main.c
+++ c/01_basics/main.c
@@ -2,11 +2,20 @@
 
 // Function declaration
 int celsius_to_fahrenheit(int celsius);
+int farenheit_to_celsius(int farenheit);
 void print_multiplication_table(int number);
 
 int main() {
-    int temp_c = 25;
+    int temp_c;
+    int temp_f;
+
+    printf("Enter temperature in Celsius:");
+    scanf("%d", &temp_c);
     printf("Temperature: %d°C = %dF°\n", temp_c, celsius_to_fahrenheit(temp_c));
+
+    printf("Enter temperature in Farenheit:");
+    scanf("%d", &temp_f);
+    printf("Temperature %d°F = %dC°\n", temp_f, farenheit_to_celsius(temp_f));
 
     printf("\nMultiplication Table for 5:\n");
     print_multiplication_table(5);
@@ -16,6 +25,10 @@ int main() {
 
 int celsius_to_fahrenheit(int celsius) {
     return (celsius * 9 / 5) + 32;
+}
+
+int farenheit_to_celsius(int farenheit) {
+    return (farenheit - 32) / 9 * 5;
 }
 
 void print_multiplication_table(int number) {
